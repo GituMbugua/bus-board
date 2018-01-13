b706eae6c8183740c1cf9e2dc2d5ed0990e4a911
Gitu Mbugua <gmbugua38@gmail.com>
2018-01-13 10:15:27 +0300

    initial commit; project structure

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gitu_m/Desktop/school/core/pro-dev/bus-board/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
