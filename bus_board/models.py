aff620e53646f28dcbb626c0546145a30dbac741
Carol Wanjohi <wanjohi.carol54@gmail.com>
2018-01-14 12:08:29 +0300

    Create Route class * Create Route class to define bus routes * Create test case for the instance of Route class * Create test case for the get routes class method to get all the bus routes in the database * Create test case for the get single route class method to get a specific bus route from the database * Create migration to a database schema with Route table

--- bus_board/models.py
+++ bus_board/models.py
@@ -39,3 +39,115 @@ class BusOrganisation(models.Model):
 
         return single_bus_origanisation
 
+class Route(models.Model):
+    '''
+    Class to define a bus route
+    '''
+    departure_location = models.CharField(max_length=255)
+
+    destination_location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.departure_location + '-' + self.destination_location
+
+    @classmethod
+    def get_routes(cls):
+        '''
+        Function to get all the bus routes in the database
+
+        Return
+            routes : list of all the Route objects in the database
+        '''
+        routes = cls.objects.all()
+
+        return routes
+
+    @classmethod
+    def get_single_route(cls, route_id):
+        '''
+        Function to get a bus route with the specific id 
+
+        Args
+            route_id : the bus route id 
+
+        Return
+            single_route : Route object with the specified id 
+        '''
+        single_route = cls.objects.get(id=route_id)
+
+        return single_route
+
+class Bus(models.Model):
+    '''
+    Class to define a bus 
+    '''
+    bus_organisation = models.ForeignKey(BusOrganisation, on_delete=models.CASCADE)
+
+    number_plate = models.CharField(max_length=50)
+
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+
+    capacity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.bus_organisation.name + ' Bus No.' + self.id
+
+    @classmethod
+    def get_buses(cls):
+        '''
+        Function to get all the buses in the database
+
+        Return
+            buses : list of all the Bus objects in the database
+        '''
+        buses = cls.objects.all()
+
+        return buses
+
+    @classmethod
+    def get_single_bus(cls, bus_id):
+        '''
+        Function to get a bus with the specific id 
+
+        Args
+            bus_id : the bus id 
+
+        Return
+            single_bus : Bus object with the specified id 
+        '''
+        single_bus = cls.objects.get(id=bus_id)
+
+        return single_bus
+
+    @classmethod
+    def get_bus_organisation_buses(cls, bus_organisation_id):
+        '''
+        Function to get buses belonging to the bus organisation with the specific id 
+
+        Args
+            bus_organisation_id : the bus organisation id 
+
+        Return
+            bus_origanisation_buses : list of all the Bus objects in the database with the specified bus organisation id
+        '''
+        bus_origanisation_buses = cls.objects.filter(bus_organisation=bus_organisation_id)
+
+        return bus_origanisation_buses
+
+    @classmethod
+    def get_route_buses(cls, route_id):
+        '''
+        Function to get buses belonging to the bus route with the specific id 
+
+        Args
+            route_id : the route id 
+
+        Return
+            route_buses : list of all the Bus objects in the database with the specified route id
+        '''
+        route_buses = cls.objects.filter(route=route_id)
+
+        return route_buses
+
+
+
--- bus_board/tests.py
+++ bus_board/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from .models import BusOrganisation
+from .models import BusOrganisation, Route, Bus
 
 # Create your tests here.
-class BusOrganisationeTestClass(TestCase):
+class BusOrganisationTestClass(TestCase):
     '''
     Test case for the Bus Organisation class
     '''
@@ -50,6 +50,186 @@ class BusOrganisationeTestClass(TestCase):
         # bus_organisations = BusOrganisation.objects.all()
         
         self.assertTrue( isinstance(gotten_bus, BusOrganisation))
+
+class RouteTestClass(TestCase):
+    '''
+    Test case for the Route class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Route class
+        '''
+        # Create instance of Route class
+        self.new_route = Route(departure_location='Nairobi', destination_location='Mombasa')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_route in an instance of Route class
+        '''
+        self.assertTrue( isinstance(self.new_route, Route) )
+
+    def test_get_routes(self):
+        '''
+        Test to check if all routes are gotten from the database
+        '''
+        self.new_route.save()
+
+        test_route = Route(departure_location='Nakuru', destination_location='Nairobi')
+
+        test_route.save()
+
+        gotten_routes = Route.get_routes()
+
+        routes = Route.objects.all()
+
+        self.assertTrue( len(gotten_routes) == len(routes) )
+
+    def test_get_single_route(self):
+        '''
+        Test to check if the specified route is gotten from the database
+        '''
+        self.new_route.save()
+
+        test_route = Route(departure_location='Naivasha', destination_location='Nairobi')
+
+        test_route.save()
+
+        gotten_route= Route.get_single_route(self.new_route.id)
+
+        # routes = Route.objects.all()
+        
+        self.assertTrue( isinstance(gotten_route, Route))
+
+class BusTestClass(TestCase):
+    '''
+    Test case for the Bus class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Bus class
+        '''
+        # Create instance of Bus Organisation class
+        test_bus_organisation = BusOrganisation(name='Kiki And Son ltd')
+
+        test_bus_organisation.save()
+
+        # Create instance of Route class
+        test_route = Route(departure_location='Nairobi', destination_location='Mombasa')
+
+        test_route.save()
+
+        # Create instance of Bus class
+        self.new_bus = Bus(bus_organisation=test_bus_organisation, number_plate='KBC 243J', route=test_route, capacity=34 )
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_bus in an instance of Route class
+        '''
+        self.assertTrue( isinstance(self.new_bus, Bus) )
+
+    def test_get_buses(self):
+        '''
+        Test to check if all buses are gotten from the database
+        '''
+        self.new_bus.save()
+
+        test_bus_organisation2 = BusOrganisation(name="Another Bus Ltd")
+
+        test_bus_organisation2.save()
+
+        test_route2 = Route(departure_location='Nakuru', destination_location='Nairobi')
+
+        test_route2.save()
+
+        test_bus = Bus(bus_organisation=test_bus_organisation2, number_plate='KCJ 523T', route=test_route2, capacity=44 )
+
+        test_bus.save()
+
+        gotten_buses = Bus.get_buses()
+
+        buses = Bus.objects.all()
+
+        self.assertTrue( len(gotten_buses) == len(buses) )
+
+    def test_get_single_bus(self):
+        '''
+        Test to check if the specified bus is gotten from the database
+        '''
+        self.new_bus.save()
+
+        test_bus_organisation2 = BusOrganisation(name="Another Bus Ltd")
+
+        test_bus_organisation2.save()
+
+        test_route2 = Route(departure_location='Nakuru', destination_location='Nairobi')
+
+        test_route2.save()
+
+        test_bus = Bus(bus_organisation=test_bus_organisation2, number_plate='KCJ 523T', route=test_route2, capacity=44 )
+
+        test_bus.save()
+
+        gotten_bus = Bus.get_single_bus(self.new_bus.id)
+
+        # buses = Bus.objects.all()
+        
+        self.assertTrue( isinstance(gotten_bus, Bus))
+
+    def test_get_bus_organisation_buses(self):
+        '''
+        Test to check if all buses for a specific bus organisation are gotten from the database
+        '''
+        self.new_bus.save()
+
+        test_bus_organisation2 = BusOrganisation(name="Another Bus Ltd")
+
+        test_bus_organisation2.save()
+
+        test_route2 = Route(departure_location='Nakuru', destination_location='Nairobi')
+
+        test_route2.save()
+
+        test_bus = Bus(bus_organisation=test_bus_organisation2, number_plate='KCJ 523T', route=test_route2, capacity=44 )
+
+        test_bus.save()
+
+        test_bus2 = Bus(bus_organisation=test_bus_organisation2, number_plate='KBY 312T', route=test_route, capacity=38 )
+
+        test_bus2.save()
+
+        gotten_bus= Bus.get_bus_organisation_buses(self.test_bus_organisation2.id)
+
+        buses = Bus.objects.all()
+
+        self.assertTrue( len(gotten_buses) != len(buses) )
+
+    def test_get_route_buses(self):
+        '''
+        Test to check if all buses for a specific bus route are gotten from the database
+        '''
+        self.new_bus.save()
+
+        test_bus_organisation2 = BusOrganisation(name="Another Bus Ltd")
+
+        test_bus_organisation2.save()
+
+        test_route2 = Route(departure_location='Nakuru', destination_location='Nairobi')
+
+        test_route2.save()
+
+        test_bus = Bus(bus_organisation=test_bus_organisation2, number_plate='KCJ 523T', route=test_route2, capacity=44 )
+
+        test_bus.save()
+
+        test_bus2 = Bus(bus_organisation=test_bus_organisation, number_plate='KBY 312T', route=test_route2, capacity=38 )
+
+        test_bus2.save()
+
+        gotten_bus= Bus.get_route_buses(self.test_route2.id)
+
+        buses = Bus.objects.all()
+
+        self.assertTrue( len(gotten_buses) != len(buses) )
 
 
 
