0fa7e54dee3625826c9bd4d9f818500498f2db35
Carol Wanjohi <wanjohi.carol54@gmail.com>
2018-01-14 17:11:24 +0300

    Create Schedule class * Create Schedule class for a bus' schedule * Create test case for the instance of Schedule class * Create test case for get schedules class method that gets all the schedules in the database * Create test case for get single schedule class method that gets a specific schedule from the database * Create test case for get bus schedules class method that gets all the schedules for a specific bus * Create test case for get travel estimation class method that calculates the estimated duration for a schedule * Create migration to a database schema with Schedule table

--- bus_board/models.py
+++ bus_board/models.py
@@ -148,7 +148,81 @@ class Bus(models.Model):
         route_buses = cls.objects.filter(route=route_id)
 
         return route_buses
-        
+
+class Schedule(models.Model):
+    '''
+    Class to define a bus schedule
+    '''
+    departure_time = models.DateTimeField(auto_now_add=False)
+
+    arrival_time = models.DateTimeField(auto_now_add=False)
+
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bus.bus_organisation.name + 'Bus No.' + self.bus.id + 'Schedule No.' + self.id
+
+    @classmethod
+    def get_schedules(cls):
+        '''
+        Function to get all the bus schedules in the database
+
+        Return
+            schedules : list of all the Schedule objects in the database
+        '''
+        schedule = cls.objects.all()
+
+        return schedule
+
+    @classmethod
+    def get_single_schedule(cls, schedule_id):
+        '''
+        Function to get a bus schedule with the specific id 
+
+        Args
+            schedule_id : the bus schedule id 
+
+        Return
+            single_schedule : Schedule object with the specified id 
+        '''
+        single_schedule = cls.objects.get(id=schedule_id)
+
+        return single_schedule
+
+    @classmethod
+    def get_bus_schedules(cls, bus_id):
+        '''
+        Function to get schedules belonging to the bus with the specific id 
+
+        Args
+            bus_id : the bus id 
+
+        Return
+            bus_schedules : list of all the Schedule objects in the database with the specified bus id
+        '''
+        bus_schedules = cls.objects.filter(bus=bus_id)
+
+        return bus_schedules
+
+    @classmethod
+    def get_travel_estimation(cls, schedule_id):
+        '''
+        Function to get the travel time estinmation for a bus schedule with the specific id 
+
+        Args
+            schedule_id : the bus schedule id 
+
+        Return
+            travel_estimation : Travel time estimation for the specified schedule
+        '''
+        schedule = cls.objects.get(id=schedule_id)
+
+        calculate_travel_estimation = schedule.arrival_time - schedule.departure_time
+
+        travel_estimation = str(calculate_travel_estimation.seconds//3600) + ' hours ' + str(calculate_travel_estimation.seconds//60 % 60) + ' minutes'
+
+        return travel_estimation
+
 
 
 
