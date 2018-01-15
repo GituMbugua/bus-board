4e38b241c2b685be038d465ffbd21b16f85d84de
John <jonnygovish@gmail.com>
2018-01-15 09:06:26 +0300

    Add view function for home and search

--- bus_board/views.py
+++ bus_board/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def search_results(request):
+  return render(request, 'search.html')
